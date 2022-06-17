5d75be602492587328faaa91e572684dabe81e52
Oleksii Verbytskyi <alverba88@gmail.com>
2022-06-17 22:10:38 +0300

    mod list

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,27 +1,15 @@
 import styles from './ContactList.module.css';
 
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  deleteContact,
-  getContacts,
-  getFilter,
-} from '../../redux/contactsSlise';
 import {
   useGetContactsQuery,
   useDeleteContactMutation,
 } from '../../redux/contactsApi';
-import { contactsApi } from '../../redux/contactsApi';
 
 export const ContactList = () => {
-  // const contacts = useSelector(getContacts);
-  // const dispatch = useDispatch();
-  // const filterValue = useSelector(getFilter);
   const filterValue = '';
   const { data: contacts, isLoading } = useGetContactsQuery();
   const [deleteContact] = useDeleteContactMutation();
-  console.log(contacts);
-  console.log(isLoading);
-  // const contacts = {};
+
   const filteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filterValue.toLowerCase())
